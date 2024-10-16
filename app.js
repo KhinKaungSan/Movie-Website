ac2e4c1d37037043e38492f581717d2400c926d6
KhinKaungSan <khinsan139@gmail.com>
2024-10-16 09:07:54 +0630

    scroll column

--- app.js
+++ app.js
@@ -5,30 +5,31 @@ arrows.forEach((arrow, i) => {
     const itemNumber = movieLists[i].querySelectorAll("img").length;
     let clickCounter = 0;
     arrow.addEventListener("click", () => {
+        const ratio = Math.floor(window.innerWidth / 270);
         clickCounter++;
-
-        // Check if there are enough items to scroll
-        if (itemNumber - (4 + clickCounter) >= 0) {
-            // Check if transform exists before accessing the x property
+        if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
             const transformMap = movieLists[i].computedStyleMap().get("transform");
             
-            let currentTranslateX = 0;  // Default value if transform is not set
+            let currentTranslateX = 0; 
 
             if (transformMap) {
-                // Access the first matrix (if it exists) and get the x value
                 currentTranslateX = transformMap[0].x.value;
             }
 
             movieLists[i].style.transform = `translateX(${currentTranslateX - 300}px)`;
         } else {
-            // Reset to initial position
             movieLists[i].style.transform = "translateX(0)";
             clickCounter = 0;
         }
     });
-
-    console.log(movieLists[i].querySelectorAll("img").length);
+    
+    console.log(Math.floor(window.innerWidth / 270));
 });
+
+
+
+
+//TOGGLE
 
 const ball = document.querySelector(".toggle-ball");
 const items = document.querySelector(".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle,.toggle-ball");
@@ -37,4 +38,5 @@ ball.addEventListener("click",()=>{
     items.forEach(item=>{
         items.classList.toggle("active")
     })
+    ball.classList.toggle("active")
 })
